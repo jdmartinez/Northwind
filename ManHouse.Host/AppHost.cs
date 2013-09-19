eb41f1d84dad0ee2954c9e8aeac644bee8a41005
laudrup69 <laudrup69@gmail.com>
2013-09-19 09:53:04 +0200

    Generación ManHouse Host

---
+++ ManHouse.Host/AppHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using ServiceStack.Api.Swagger;
+using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.Common.Utils;
+using ServiceStack.Logging;
+using ServiceStack.Logging.Support.Logging;
+using ServiceStack.OrmLite;
+//using ServiceStack.OrmLite.Sqlite;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface.Cors;
+using ServiceStack.ServiceInterface.Validation;
+using ServiceStack.Text;
+using ServiceStack.WebHost.Endpoints;
+//using ServiceStack.Razor;
+
+namespace ManHouse.Host
+{
+    /// <summary>
+    /// Clase que representa la aplicación Web
+    /// </summary>
+    public class AppHost : AppHostBase
+    {
+//        public AppHost() : base("ManHouse web services",
+//            base("Northwind web services", typeof(CustomersService).Assembly)
+    }
+}
